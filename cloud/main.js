8fc3c29d0a0e3002376ae8577530ba0fb412d210
Oleksandr Malovichko <a@madappgang.com>
2017-05-17 15:16:14 +0300

    Default role for users

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,19 @@
-
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.afterSave(Parse.User, function(request) {
+  var user = request.object;
+  var query = new Parse.Query(Parse.Role);
+  query.equalTo("name", "user");
+  query.first ( {
+      useMasterKey: true,
+      success: function(object) {
+          if ( object ) {
+              object.relation("users").add(user);
+              object.save(null, { useMasterKey: true });
+              console.log("Add user to role user");
+          }
+          consloe.log(object);
+      },
+      error: function(error) {
+          console.log("Add user to role error:  " + error.message);
+      }
+  });
 });
